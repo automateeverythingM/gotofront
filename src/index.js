4c5ad49d22ef98df4cd90f97f93fc0b6658276d9
automateeverythingM <markospavicbg@gmail.com>
2021-04-15 15:23:47 +0200

    back

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
 import { io } from "socket.io-client";
-import ErrorBoundary from "./components/ErrorBoundries";
 export const socket = io("https://gotoexpress.herokuapp.com/");
 
 ReactDOM.render(
     <Provider store={store}>
-        <ErrorBoundary>
-            <App />
-        </ErrorBoundary>
+        <App />
     </Provider>,
     document.getElementById("root")
 );
